da816d87ae22711284ecd32abc54d8cd4d43faee
Mattia46 <mattia.assogna@gmail.com>
2016-06-16 15:19:14 +0100

    further updates

--- test/unit/appSpec.js
+++ test/unit/appSpec.js
@@ -1,16 +1,4 @@
-describe('appNameConrtoller', function() {
+describe('mainCtrl', function() {
   beforeEach(module('appName'));
 
-  var $controller;
-
-  beforeEach(inject(function(_$controller_){
-    $controller = _$controller_;
-  }));
-
-  describe('first test', function(){
-    it('should do something', function(){
-      var controller = $controller('appNameController');
-    });
-
-  });
 });
--- test/unit/mainCtrlSpec.js
+++ test/unit/mainCtrlSpec.js
@@ -2,11 +2,9 @@ describe('test getting data', function(){
 
   beforeEach(module('appName'));
 
-  var $controller;
+  var $httpBackend;
 
   beforeEach(inject(function(_$controller_, _$httpBackend_) {
-    $controller = _$controller_;
-    $scope = {};
     $httpBackend = _$httpBackend_;
   }));
 
